c5ab3897b538bcf95134000edade3a8d118637fc
ShopEase Dev <shopease@example.com>
2026-04-05 18:31:41 +0530

    Your commit message

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // adjust this if needed
+    baseURL: import.meta.env.VITE_BASE_URL, // ✅ FIXED
 });
 
-// Request interceptor to add the auth token header to requests
 api.interceptors.request.use(
     (config) => {
         const userInfo = sessionStorage.getItem('userInfo');
